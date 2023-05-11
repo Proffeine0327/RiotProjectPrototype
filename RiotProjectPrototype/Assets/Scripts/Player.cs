a678bf1697642b25ab7cc97cd6d7916448f782b2
proffeine0327 <proffeine0327@gmail.com>
2023-05-12 00:23:18 +0900

    basic player movement, add some obstacles

--- RiotProjectPrototype/Assets/Scripts/Player.cs
+++ RiotProjectPrototype/Assets/Scripts/Player.cs
@@ -9,15 +9,22 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float gravityScale;
     [SerializeField] private float jumpScale;
+    [Header("aim")]
+    [SerializeField] private Vector2 aimSensivity;
+    [SerializeField] private Camera mainCam;
 
     private CharacterController cc;
     private float yVelocity;
+    private Vector2 aimVec;
 
     private void Awake() 
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
         player = this;
 
-        cc.GetComponent<CharacterController>();    
+        cc = GetComponent<CharacterController>();    
     }
 
     private void Update() 
@@ -34,11 +41,13 @@ public class Player : MonoBehaviour
         var velocity = new Vector3(h, 0, v);
         velocity = transform.TransformDirection(velocity).normalized * moveSpeed;
 
-        if(!cc.isGrounded)
-        {
-            yVelocity -= 9.8f * gravityScale * Time.deltaTime;
-            velocity.y = yVelocity;
-        }
+        if(!cc.isGrounded) yVelocity -= 9.8f * gravityScale * Time.deltaTime;
+        else yVelocity = 0;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            yVelocity = jumpScale;
+
+        velocity.y = yVelocity;
 
         cc.Move(velocity * Time.deltaTime);
     }
@@ -47,5 +56,12 @@ public class Player : MonoBehaviour
     {
         var mouseX = Input.GetAxis("Mouse X");
         var mouseY = Input.GetAxis("Mouse Y");
+
+        aimVec.x += mouseX * aimSensivity.x * Time.deltaTime;
+        aimVec.y -= mouseY * aimSensivity.y * Time.deltaTime;
+        aimVec.y = Mathf.Clamp(aimVec.y, -85, 85);
+
+        transform.rotation = Quaternion.Euler(0, aimVec.x, 0);
+        mainCam.transform.localRotation = Quaternion.Euler(aimVec.y, 0, 0);
     }
 }
